63d2f9a809d06b01f5ab753b3b53e32ed451ae4a
vishalkrverma <vishalkrverma302004@gmail.com>
2025-06-26 08:37:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Game with String/game-with-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minValue(String s, int k) {
+        // code here
+        Map<Character,Integer> map=new TreeMap<>();
+        
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        
+        PriorityQueue<Integer> maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int freq:map.values()){
+            maxHeap.offer(freq);
+        }
+        
+        while(k>0 && !maxHeap.isEmpty()){
+            int top=maxHeap.poll();
+            top--;
+            k--;
+            
+            if(top>0){
+                maxHeap.offer(top);
+            }
+        }
+        int sum=0;
+        while(!maxHeap.isEmpty()){
+            int val=maxHeap.poll();
+            sum+=val*val;
+        }
+        return sum;
+        
+    }
+}
