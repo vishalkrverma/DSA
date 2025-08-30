618013ddbc19641684ed44a83e69fb54f2dbee02
Vishal Kr Verma <vishalkrverma302004@gmail.com>
2025-08-30 07:14:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Smallest window containing 0, 1 and 2/smallest-window-containing-0-1-and-2.java
+++ Difficulty: Easy/Smallest window containing 0, 1 and 2/smallest-window-containing-0-1-and-2.java
@@ -1,27 +1,28 @@
-import java.util.HashMap;
+// User function Template for Java
 
 class Solution {
     public int smallestSubstring(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        int cnt = Integer.MAX_VALUE;
-        int j = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-
-            while (map.size() == 3) {
-                cnt = Math.min(cnt, i - j + 1);
-
-                char leftChar = s.charAt(j);
-                map.put(leftChar, map.get(leftChar) - 1);
-                if (map.get(leftChar) == 0) {
-                    map.remove(leftChar);
+      
+        HashMap<Character,Integer> map=new HashMap<>();
+        
+        int cnt=Integer.MAX_VALUE;
+        int j=0;
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            
+            while(map.size()==3){
+                char temp=s.charAt(j);
+                map.put(temp,map.get(temp)-1);
+                 cnt=Math.min(cnt,i-j+1);
+                
+                if(map.get(temp)==0){
+                    map.remove(temp);
                 }
                 j++;
             }
         }
-
-        return (cnt == Integer.MAX_VALUE) ? -1 : cnt;
+        return cnt==Integer.MAX_VALUE?-1:cnt;
     }
-}
+};
