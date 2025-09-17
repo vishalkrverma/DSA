2c3b09f7ed619b3a943cee1d274b0141e1b37dec
Vishal Kr Verma <vishalkrverma302004@gmail.com>
2025-09-17 06:45:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Case-specific Sorting of Strings/casespecific-sorting-of-strings.java
+++ Difficulty: Medium/Case-specific Sorting of Strings/casespecific-sorting-of-strings.java
@@ -1,35 +1,37 @@
-import java.util.Arrays;
-
 class Solution {
-    public static String caseSort(String s) {
-        StringBuilder upperCase = new StringBuilder();
-        StringBuilder lowerCase = new StringBuilder();
-
-        for (char ch : s.toCharArray()) {
-            if (Character.isUpperCase(ch)) {
-                upperCase.append(ch);
-            } else if (Character.isLowerCase(ch)) {
-                lowerCase.append(ch);
+    public static String caseSort(String str) {
+        // code here
+        int n=str.length();
+        StringBuilder small=new StringBuilder();
+        StringBuilder cap=new StringBuilder();
+        
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)>='A' && str.charAt(i)<='Z'){
+                cap.append(str.charAt(i));
+            }
+            else{
+                small.append(str.charAt(i));
             }
         }
-
-        char[] upper = upperCase.toString().toCharArray();
-        char[] lower = lowerCase.toString().toCharArray();
-
-        Arrays.sort(upper);
-        Arrays.sort(lower);
-
-        int upIndex = 0, lowIndex = 0;
-        StringBuilder result = new StringBuilder();
-
-        for (char ch : s.toCharArray()) {
-            if (Character.isUpperCase(ch)) {
-                result.append(upper[upIndex++]);
-            } else if (Character.isLowerCase(ch)) {
-                result.append(lower[lowIndex++]);
+        char[] smallLetter=small.toString().toCharArray();
+        char[] capital=cap.toString().toCharArray();
+        
+        Arrays.sort(smallLetter);
+        Arrays.sort(capital);
+        StringBuilder sb=new StringBuilder();
+        int j=0;
+        int k=0;
+        
+        for(int i=0;i<n;i++){
+            char ch=str.charAt(i);
+            
+            if(ch>='A' && ch<='Z'){
+                sb.append(capital[k++]);
+            }
+            else{
+                sb.append(smallLetter[j++]);
             }
         }
-
-        return result.toString();
+        return sb.toString();
     }
 }
