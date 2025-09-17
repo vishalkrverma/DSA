79a41e44d7840bf5918fb1b4bda56691c5e2a109
Vishal Kr Verma <vishalkrverma302004@gmail.com>
2025-09-17 11:26:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/String formation from substring/string-formation-from-substring.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+
+class Solution {
+    int isRepeat(String s) {
+        int n = s.length();
+        for (int len = 1; len <= n / 2; len++) {
+            if (n % len == 0) {
+                String pattern = s.substring(0, len);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < n / len; i++) {
+                    sb.append(pattern);
+                }
+                if (sb.toString().equals(s)) {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+}
